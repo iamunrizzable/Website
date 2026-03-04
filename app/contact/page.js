8c80a2476c33a39b3dee6ac83324caec467e1eab
iamunrizzable <Tyler@tjbmanagementinc.com>
2026-03-04 08:02:42 -0500

    Change menu link from Meet Tyler to I'm Tyler

--- app/contact/page.js
+++ app/contact/page.js
@@ -307,7 +307,7 @@ export default function Contact() {
       <div className={`menu-dropdown${menuOpen ? ' active' : ''}`}>
         <a href="/" onClick={() => setMenuOpen(false)}>Home</a>
         <a href="/hallie" onClick={() => setMenuOpen(false)}>Meet Hallie</a>
-        <a href="/tyler" onClick={() => setMenuOpen(false)}>Meet Tyler</a>
+        <a href="/tyler" onClick={() => setMenuOpen(false)}>I'm Tyler</a>
         <a href="/contact" onClick={() => setMenuOpen(false)}>Contact</a>
         <a href="/legal" onClick={() => setMenuOpen(false)}>Legal & Guidelines</a>
       </div>
