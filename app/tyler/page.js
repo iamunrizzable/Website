2f96dd059bdbb8f2595c9de1111393a8e7b52d17
iamunrizzable <Tyler@tjbmanagementinc.com>
2026-03-04 08:03:54 -0500

    Fix background image parallax scrolling - remove background-attachment fixed and adjust sizing

--- app/tyler/page.js
+++ app/tyler/page.js
@@ -44,11 +44,10 @@ export default function Tyler() {
           position: fixed;
           top: 0;
           left: 0;
-          width: 100vw;
-          height: 100vh;
+          width: 100%;
+          height: 100%;
           background-image: linear-gradient(rgba(15, 23, 42, 0.85), rgba(15, 23, 42, 0.85)), url("/bg-tyler.jpg");
           background-size: cover;
-          background-attachment: fixed;
           background-position: center center;
           background-repeat: no-repeat;
           z-index: -3;
